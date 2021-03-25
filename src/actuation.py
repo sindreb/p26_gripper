3803a8db17cfdf97e140fb9e67d5fd6768ba6257
sindreb <sindre.bokneberg@hotmail.com>
2021-03-25 14:01:36 +0100

    Averaging of current sensor, add comments

--- src/actuation.py
+++ src/actuation.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3.7
 
-import rospy
 import time
 import numpy as np
-from std_msgs.msg import String
 import RPi.GPIO as GPIO
 from pyfirmata import Arduino, util
 
-board = Arduino('/dev/ttyACM0')
-it = util.Iterator(board)
+# Setup for communication with Arduino
+board = Arduino('/dev/ttyACM0') # Define device connection
+it = util.Iterator(board)   # Receive data into iterator
 it.start()
 time.sleep(0.1)
-current_sense = board.analog[0]
-current_sense.enable_reporting()
+current_sense = board.analog[0] # Define analog pin 0
+current_sense.enable_reporting() # Start reporting from current_sense
 
 
 # Setup for GPIO on Raspberry
@@ -22,10 +21,10 @@ GPIO.setup(33, GPIO.OUT) # Set pin 33 as output
 GPIO.setup(11, GPIO.OUT) # Set pin 11 as output
 GPIO.setup(13, GPIO.OUT) # Set pin 13 as output
 
-motor = GPIO.PWM(33, 500) # Set pin 33 as motor PWM output at 10000 Hz
+motor = GPIO.PWM(33, 18000) # Set pin 33 as motor PWM output at 10000 Hz
 
-max_opening_current = 0.01 # Maximum current for motor at opening
-max_closing_current = 0.005 # Maximum current for motot at closing
+max_opening_current = 0.013 # Maximum current for motor at opening
+max_closing_current = 0.01 # Maximum current for motot at closing
 
 
 def CW(speed):
@@ -38,14 +37,11 @@ def CW(speed):
 
 def CCW(speed):
     motor.stop()
-    GPIO.output(11, GPIO.LOW)
     GPIO.output(13, GPIO.HIGH)
+    GPIO.output(11, GPIO.LOW)
     motor.start(0)
-    for dc in np.arange(0, speed, 1/speed):
-        motor.ChangeDutyCycle(dc)
-    #    print(dc)
-    #    print(current_sense.read())
-        time.sleep(0.1/speed)
+    motor.ChangeDutyCycle(speed)
+    
 
 def sleep(sec):
     for sleep in np.arange(0, sec, 0.2):
@@ -53,19 +49,16 @@ def sleep(sec):
         time.sleep(0.2)
     board.analog[0].disable_reporting()    
 
-def current_average():
+def current_average(): # Finds and calculates average current
     current_it = 0
-    for current_count in np.arange(0, 40, 1):
+    for current_count in np.arange(1, 40, 1): # 40 measurements
         current_it += current_sense.read()
-        time.sleep(0.1/40)
+        time.sleep(0.1/40) # Time between each measurement
  
-    current_avg = np.round(current_it/40, 5)
-    #print(current_avg)
+    current_avg = np.round(current_it/40, 5) # Average of 40 measurement, rounded to 5 decimals
     return current_avg
 
-
-def hold():
-    motor.stop()
+def hold(): # Holds torque at the motor by brake to ground
     GPIO.output(11, GPIO.LOW)
     GPIO.output(13, GPIO.LOW)
     motor.start(0)
@@ -74,19 +67,38 @@ def open():
     print("Opening gripper...")
     CW(5)
     current_avg = 0
-    while current_sense.read() == None:
+    current_max_count = 0
+    while current_sense.read() == None: # Passes through initial None value
         pass
 
-    while current_avg < max_opening_current:
+    while current_max_count < 3:
+        if current_avg > max_opening_current:
+            current_max_count += 1
+    
         current_avg = current_average()
         print(current_avg)
-
     hold()
 
-#while True:
+def close():
+    print("Closing gripper...")
+    CCW(5)
+    current_avg = 0
+    while current_sense.read() == None: # Passes through initial None value
+        pass
 
-#    open()
-#    time.sleep(5)
+    while current_avg < max_closing_current:
+        current_avg = current_average()
+        print(current_avg)
+    hold()
+
+
+while True:
+
+    open()
+    time.sleep(5)
+
+
+
     #CW(5)
     #for freq in np.array([100, 500, 1000, 2000, 4000, 5000, 7000, 10000, 12000, 15000, 20000]):
     #    motor.ChangeFrequency(freq)
--- src/gripping.py
+++ src/gripping.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 
 import rospy
 from std_msgs.msg import String, Int64, Float32
+import time
+from actuation import open, close, hold
 
-pub = rospy.Publisher('p26_lefty/gripper', Int64, queue_size=10)
+pub = rospy.Publisher('p26_lefty/gripper', Int64, queue_size=100)
 
 def grip_procedure(data):
     global actuation
     actuation = data.data
 
 def open_grip():
-    #Opening gripper
+    open()
+    hold()
     gripper_pos = 1 #1 open, 2 closed
     rospy.loginfo("Gripper open")
     pub.publish(gripper_pos)
 
 def close_grip():
-    #Closing gripper
+    close()
+    hold()
     gripper_pos = 2 #1 open, 2 closed
     rospy.loginfo("Gripper closed")
     pub.publish(gripper_pos)
@@ -27,16 +31,15 @@ def main():
     rate = rospy.Rate(10)
     rospy.Subscriber("p26_lefty/gripper", Int64, grip_procedure)
     rospy.loginfo("Gripper ready for input...")
+    hold()
     rospy.wait_for_message("p26_lefty/gripper", Int64, timeout=None)
 
     if actuation == 1:
         rospy.loginfo("Opening gripper...")
-        rospy.sleep(2)
         open_grip()
 
     elif actuation == 2:
         rospy.loginfo("Closing gripper...")
-        rospy.sleep(2)
         close_grip()
 
     elif actuation != 1 or actuation != 2:
